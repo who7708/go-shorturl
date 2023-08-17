cc55abcc3a2f7e714fed406408519b3fdd657b01
Chris <who7708@163.com>
2023-08-17 21:56:41 +0800

    单元测试

---
+++ ch02/app_test.go
@@ -0,0 +1,102 @@
+package main_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	"github.com/stretchr/testify/mock"
+)
+
+const (
+	expTime       = 60
+	longURL       = "https://www.baidu.com"
+	shortUrl      = "Hello"
+	shortLinkInfo = `{ "url":"https://www.baidu.com", "create_at":"2023-08-17 21:17:12.34971 +0800 CST m=+62.079104201", "expiration_in_minutes":1 }`
+)
+
+type storageMock struct {
+	mock.Mock
+}
+
+var app main.App
+var mockR *storageMock
+
+func (s *storageMock) Shorten(url string, expirationInMinutes int64) (string, error) {
+	args := s.Called(url, expirationInMinutes)
+	return args.String(0), args.Error(1)
+}
+
+func (s *storageMock) ShortlinkInfo(shortUrl string) (interface{}, error) {
+	args := s.Called(shortUrl)
+	return args.String(0), args.Error(1)
+}
+
+func (s *storageMock) Unshortend(shortUrl string) (string, error) {
+	args := s.Called(shortUrl)
+	return args.String(0), args.Error(1)
+}
+
+func init() {
+	app = main.App{}
+	mockR = new(storageMock)
+	app.Initialize(&main.Env{S: mockR})
+}
+
+func TestCrateShortLink(t *testing.T) {
+	var jsonStr = []byte(`{
+    "url": "https://www.baidu.com",
+    "expiration_in_minutes": 60
+}`)
+
+	req, err := http.NewRequest("POST", "/api/shorten", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		t.Fatal("无法创建请求", err)
+	}
+	req.Header.Set("Content-TYpe", "application/json")
+
+	// mock
+	mockR.On("Shorten", longURL, int64(expTime)).Return(shortUrl, nil).Once()
+
+	rw := httptest.NewRecorder()
+	app.Router.ServeHTTP(rw, req)
+	if rw.Code != http.StatusCreated {
+		t.Fatal("期望值：%d, 实际: %d", http.StatusCreated, rw.Code)
+	}
+
+	resp := struct {
+		ShortLink string `json:"shortlink"`
+	}{}
+	if err := json.NewDecoder(rw.Body).Decode(&resp); err != nil {
+		t.Fatal("解码错误")
+	}
+
+	if resp.ShortLink != shortUrl {
+		t.Fatal("期望值：%s, 实际: %s", shortUrl, resp.ShortLink)
+	}
+
+}
+
+func TestShortLinkRedirect(t *testing.T) {
+	r := fmt.Sprintf("/%s", shortUrl)
+
+	req, err := http.NewRequest("GET", r, nil)
+
+	if err != nil {
+		t.Fatal("无法创建请求", err)
+	}
+
+	mockR.On("Unshortend", shortUrl).Return(longURL, nil).Once()
+
+	rw := httptest.NewRecorder()
+
+	app.Router.ServeHTTP(rw, req)
+
+	if rw.Code != http.StatusTemporaryRedirect {
+		t.Fatal("期望值：%s, 实际: %s", http.StatusTemporaryRedirect, rw.Code)
+	}
+}
--- ch02/go.mod
+++ ch02/go.mod
@@ -3,13 +3,19 @@ module app
 go 1.19
 
 require (
+	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/gorilla/mux v1.8.0
 	github.com/justinas/alice v1.2.0
+	github.com/mattheath/base62 v0.0.0-20150408093626-b80cdc656a7a
+	github.com/stretchr/testify v1.8.4
 	gopkg.in/validator.v2 v2.0.1
 )
 
 require (
-	github.com/go-redis/redis v6.15.9+incompatible // indirect
-	github.com/mattheath/base62 v0.0.0-20150408093626-b80cdc656a7a // indirect
-	github.com/redis/go-redis v6.15.9+incompatible // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.27.10 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
