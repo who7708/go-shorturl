3447d8ba292a72d9b0a155dac67512e7819485da
Chris <who7708@163.com>
2023-08-17 20:49:08 +0800

    短链生成实现

--- ch02/redis.go
+++ ch02/redis.go
@@ -1,21 +1,24 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"time"
 
 	"github.com/go-redis/redis"
+	"github.com/mattheath/base62"
 )
 
 // 定义常量
 const (
 
-	// 生成短链key
+	// 全局自增ID
 	URLIDKEY = "next.url.id"
 
 	// 短链映射长链
 	ShortlinkKey = "shortlink:%s:url"
 
-	// 短链hash与地址进行映射
+	// 长链hash与短链进行映射
 	URLHashKey = "urlhash:%s:url"
 
 	// 短链详情
@@ -35,7 +38,7 @@ type URLDetail struct {
 
 // 初始化redis客户端
 func NewRedisCli(addr string, passwd string, db int) *RedisCli {
-	c := redis.NewClient(opt * redis.Options{
+	c := redis.NewClient(&redis.Options{
 		Addr:     addr,
 		Password: passwd,
 		DB:       db,
@@ -45,4 +48,83 @@ func NewRedisCli(addr string, passwd string, db int) *RedisCli {
 	}
 
 	return &RedisCli{Cli: c}
+}
+
+// 长链生成短链
+// Shorten(url string, expirationInMinutes int64) (string, error)
+// // 短链详情
+// ShortlinkInfo(shortUrl string) (interface{}, error)
+// // 短链还原长链
+// Unshortend(shortUrl string) (string, error)
+func (r *RedisCli) Shorten(url string, expirationInMinutes int64) (string, error) {
+	h := toSha1(url)
+
+	d, err := r.Cli.Get(fmt.Sprintf(URLHashKey, h)).Result()
+
+	if err == redis.Nil {
+
+	} else if err != nil {
+		return "", err
+	} else {
+		if d == "{}" {
+			// 过期了，nothing to do
+		} else {
+			return d, nil
+		}
+	}
+
+	// increase the global counter
+	err = r.Cli.Incr(URLIDKEY).Err()
+
+	if err != nil {
+		return "", err
+	}
+
+	id, err := r.Cli.Get(URLIDKEY).Int64()
+	if err != nil {
+		return "", err
+	}
+
+	shortUrl := base62.EncodeInt64(id)
+
+	err = r.Cli.Set(fmt.Sprintf(ShortlinkKey, shortUrl), url,
+		time.Minute*time.Duration(expirationInMinutes)).Err()
+
+	if err != nil {
+		return "", err
+	}
+
+	detail, err := json.Marshal(
+		&URLDetail{
+			URL:                 url,
+			CreateAt:            time.Now().String(),
+			ExpirationInMinutes: time.Duration(expirationInMinutes),
+		},
+	)
+
+	if err != nil {
+		return "", nil
+	}
+
+	err = r.Cli.Set(fmt.Sprintf(ShortlinkDetailKey, shortUrl), detail,
+		time.Minute*time.Duration(expirationInMinutes)).Err()
+
+	if err != nil {
+		return "", err
+	}
+
+	return shortUrl, nil
+
+}
+
+func toSha1(url string) string {
+	panic("unimplemented")
+}
+
+func (r *RedisCli) ShortlinkInfo(shortUrl string) (interface{}, error) {
+	return "", nil
+}
+
+func (r *RedisCli) Unshortend(shortUrl string) (string, error) {
+	return "", nil
 }
